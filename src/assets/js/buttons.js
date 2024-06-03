2b5c8e47b6084d6efa06e7d5fbb7cfa76f075a2e
Chromatic Vizier <Zjq8947@gmail.com>
2024-06-03 18:48:25 +0800

    feat(src, new_home.html): Completed the division of the section by adding a one-click back to the top button.

---
+++ src/assets/js/buttons.js
@@ -0,0 +1,8 @@
+function backToTop() {
+    console.log('back to top clicked')
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+}
--- src/assets/js/homePageMonitor.js
+++ src/assets/js/homePageMonitor.js
@@ -1,13 +1,9 @@
 function handleScroll() {
     let windowHeight = window.innerHeight
     let currentDistance = window.scrollY
-    if (currentDistance >= 0.5*windowHeight)
-    {
-        let tracks = document.getElementById('test-box')
-        tracks.setAttribute('class', 'test-box-to-add')
+    if (currentDistance >= 0.5*windowHeight) {
+
     }
 }
 
 window.addEventListener("scroll", handleScroll, true)
-
-// if (window.scrollY >= 0.5*window.innerHeight) console.log('0.5')
